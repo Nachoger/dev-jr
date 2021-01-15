1b695ee360e702bd6c57badb481cf58866e7d092
Nachoger <71718733+Nachoger@users.noreply.github.com>
2021-01-15 15:41:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,24 +16,12 @@
 <h2 id="A">Registro_Clientes</h2>
 
 <p id="O">
-
-id 
 <br/><input type="text" name="id" autofocus="" required=""><br/>
-
-email
 <br/><input type="text" name="email" autofocus="" required=""><br/>
-
-nombre
 <br/><input type="text" name="nombre" autofocus="" required=""><br/>
- 
-apellido
 <br/><input type="text" name="apellido" autofocus="" required=""><br/>
-
-teléfono
-<br/><input type="text" name="teléfono" autofocus="" required=""><br/>
+<br/><input type="text" name="telefono" autofocus="" required=""><br/>
 <br/>
-
-
 <input type="submit" name="Registrar" value="Registrar">
 
 </form>
